6205da0e9d66941b9e72638217ce849136d0cdec
Tobias Litherland <tobiaslland@gmail.com>
2018-06-13 14:58:08 +0200

    gitignore

--- photobook/main.py
+++ photobook/main.py
@@ -6,12 +6,15 @@ class Photobook:
         self.images = images.load(image_store)
         #self.text = text.load(text_store)
 
-    def generate_pdf(self, pdf_path) -> None:
-        doc = Document('basic')
-        with doc.create(Section('Året 2018')):
+    def generate_pdf(self, path, title) -> None:
+        print('Generating pdf!')
+        doc = Document(default_filepath=path)
+        with doc.create(Section('The year 2018')):
+            doc.append('Here are some cool images from 2018!')
             for i, image in enumerate(self.images):
-                with doc.create(Figure(position='h!')) as image:
-                    image.add_image(image.path, width='120px')
-                    image.add_caption(f'Image nr {i} taken {image.timestampstr}')
-        doc.generate_pdf
+                with doc.create(Figure(position='h!')) as figure:
+                    figure.add_image(image.path, width='400px')
+                    #figure.add_caption(f'Image nr {i} taken {image.timestampstr}')
+        doc.generate_pdf(title)
+        print('Done!')
 
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,16 @@
 from photobook.main import Photobook
+import shutil
+import os
 
 def test_main():
     book = Photobook(image_store=r"E:\Dropbox\Tobias\Programming\photobook\test\test_photos\*.jpg", text_store='somepath')
     assert len(book.images) == 3
+
+    pdf_path = r'E:\Dropbox\Tobias\Programming\photobook\test\test_photos'
+    pdf_name = 'test_pdf'
+    book.generate_pdf(path=pdf_path, title=pdf_name)
+
+    #shutil.rmtree(os.path.join(pdf_path, pdf_name+'.pdf'))
+
+
+
