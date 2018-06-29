869ead863ad075c5f012f6248536004de830de68
Tobias Litherland <tobiaslland@gmail.com>
2018-06-29 14:48:15 +0200

    First proceduraly created page

--- photobook/templates.py
+++ photobook/templates.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from photobook.main import PhotoCollection, TextCollection
+from photobook.main import PhotoCollection, TextCollection, Photo
 
 """
 Templates take a set of photos and text and create a page layout. 
@@ -25,16 +25,18 @@ class Page:
 
     image_crop_tolarence = 0.2  # 0.2 means that it tolerates 20% crop in either length or width to fit a template.
 
-    def __init__(self, height: float, width: float, unit:str,
-                 margins: float=None, left_margin: float=None, right_margin: float=None, top_margin: float=None, bottom_margin: float=None,
-                 spacing: float=0, image_crop_tolerance: float=0.2) -> None:
+    def __init__(self, height: float, width: float, unit: str,
+                 margins: float = None, left_margin: float = None, right_margin: float = None, top_margin: float = None,
+                 bottom_margin: float = None,
+                 spacing: float = 0, image_crop_tolerance: float = 0.2) -> None:
         self.height = height
         self.width = width
         self.unit = unit
         self.margins = margins
         if not isinstance(margins, type(None)):
-            left_margin=right_margin=top_margin=bottom_margin=margins
-        if all([isinstance(margin, type(None)) for margin in [margins, left_margin, right_margin, bottom_margin, top_margin]]):
+            left_margin = right_margin = top_margin = bottom_margin = margins
+        if all([isinstance(margin, type(None)) for margin in
+                [margins, left_margin, right_margin, bottom_margin, top_margin]]):
             raise TypeError('When no catch-all margins is set, you must define left/right/top/bottom_margin.')
         self.left_margin = left_margin
         self.right_margin = right_margin
@@ -44,7 +46,7 @@ class Page:
         self.image_crop_tolerance = image_crop_tolerance
 
     def width_division(self, divisions) -> float:
-        return (self.width-self.left_margin-self.right_margin)/divisions
+        return (self.width - self.left_margin - self.right_margin) / divisions
 
     def height_division(self, divisions) -> float:
         return (self.height - self.top_margin - self.bottom_margin) / divisions
@@ -57,23 +59,54 @@ class TemplateLibrary:
 
 
 class Template:
-    def __init__(self, page, photos:PhotoCollection=None, text: TextCollection=None) -> None:
+    accepts = []
+
+    def __init__(self, page, photos: PhotoCollection = None, text: TextCollection = None) -> None:
+        assert self.accepts == [photo.shape for photo in photos]
         self.page = page
         self.photos = photos
         self.text = text
 
+    def fit_photo_to_page(self, *, photo: Photo, forced_width: float=None, forced_height: float=None) -> str:
+        # Ok, so we have an image. This image is shorter than page height when the width is given (i.e. 1/3 of page width.
+        # This can be calculated quite easily by width*photo_width = height*photo_height => height = width*photo_width/photo_height
+        # If height is less than image height, we need to crop the sides of the image by a factor of:
+        # factor = page_height/photo_height
+        # This factor must be less than page.image_crop_tolarence
+        # The output must be a set of distances pr edge in page units.
+        assert forced_height or forced_width
+
+        pass
+
+    @staticmethod
+    def wrap(lines: List[str], name: str, args: List[str] = None, additional_commands: List[str]=None) -> List[str]:
+        if args:
+            arg_format = f'[{",".join(args)}]'
+        else:
+            arg_format = ''
+        if additional_commands:
+            for command in reversed(additional_commands):
+                lines.insert(0, f'\\{command}')
+
+        lines.insert(0, f'\\begin{{{name}}}' + arg_format)
+        lines.append(f'\\end{{{name}}}')
+        return lines
+
 
 class L3P(Template):  # Landscape page, 3 Portraits
     # TODO: Get this working according to the last page of album_test_modify.tex
+    accepts = ['portrait', 'portrait', 'portrait']
 
     def latex(self) -> str:
+        image_width_str = f'{self.page.width_division(3):n}{self.page.unit}'
         lines = []
-        lines.extend([r'\begin{slide}% vertically center', r'\small', r'\begin{center}'])
-        lines.extend([r'\begin{tikzpicture}[remember picture,overlay]'])
         for photo, place in zip(self.photos, ['east', 'center', 'west']):
-            # match = r'\node[anchor=east] at (current page.east){\resizebox{2cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-01-19-08.39.51}.jpg}}};'
-            lines.append(rf'\node[anchor={place}] at (current page.{place})' + r'{\resizebox{2cm}{!}' + r'{' + photo.includegraphics_latex + r'}};')
-        lines.extend([r'\end{tikzpicture}'])
-        lines.extend([r'\end{center}', r'\end{slide}'])
+            lines.append(
+                f'\\node[anchor={place}] at (current page.{place})' + f'{{\\resizebox{{{image_width_str}}}{{!}}' + f'{{{photo.includegraphics_latex}}}}};')
+
+        self.wrap(lines, name='tikzpicture', args=['remember picture', 'overlay'])
+        self.wrap(lines, name='center')
+        self.wrap(lines, name='slide', additional_commands=['small'])
+        lines.append('\clearpage')
 
         return '\n'.join(lines)
--- test/test_templates.py
+++ test/test_templates.py
@@ -6,25 +6,28 @@ def test_L3P():
 
     page = Page(height=12, width=24, unit='cm', margins=0)
 
-    layout = L3P(page=page, photos=photos)
+    layout = L3P(page=page, photos=[photos[0], photos[1], photos[0]])
 
     result = layout.latex()
 
-    match = r'''
-\begin{slide}% vertically center
+    match = r'''\begin{slide}
 \small
 \begin{center}
 \begin{tikzpicture}[remember picture,overlay]
-\node[anchor=east] at (current page.east) {\resizebox{2cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-01-19-08.39.51}.jpg}}};
-\node[anchor=center] at (current page.center) {\resizebox{2cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-02-28-09.12.22}.jpg}}};
-\node[anchor=west] at (current page.west) {\resizebox{2cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-04-08-09.24.39}.jpg}}};
+\node[anchor=east] at (current page.east){\resizebox{8cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-01-19-08.39.51}.jpg}}};
+\node[anchor=center] at (current page.center){\resizebox{8cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-02-28-09.12.22}.jpg}}};
+\node[anchor=west] at (current page.west){\resizebox{8cm}{!}{\includegraphics[angle=270]{E:/Dropbox/Tobias/Programming/photobook/test/bin/{2018-01-19-08.39.51}.jpg}}};
 \end{tikzpicture}
 \end{center}
-\end{slide}'''
+\end{slide}
+\clearpage'''
 
-    for r, m in zip(result.split('\n'), match.split('\n')):
-        print(r)
-        print(m)
+    # for r, m in zip(result.split('\n'), match.split('\n')):
+    #     print(r)
+    #     print(m)
+    #     print('\n')
 
     # TODO: Make these match
     assert result == match
+
+    print(result)
